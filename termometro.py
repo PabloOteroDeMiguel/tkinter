11f0f31fc38752a3184731b3a6672c88c2dd94a2
PabloOteroDeMiguel <potero91@gmail.com>
2020-09-28 16:13:25 +0200

    casi completo

--- termometro.py
+++ termometro.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 class mainApp(Tk):
     entrada = None
     tipoUnidad = None
+    __temperaturaAnt = ""
     
     def __init__(self):
         Tk.__init__(self)
@@ -13,15 +14,43 @@ class mainApp(Tk):
         self.resizable(0,0)
         
         self.temperatura = StringVar(value="")
+        self.temperatura.trace("w", self.validateTemperature)
         self.tipoUnidad = StringVar(value="C")
         
         self.createLayout()
     
     def createLayout(self):
-        self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)        
+        self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)
+        
+        self.lblUnidad = ttk.Label(self, text="Grados:").place(x=10, y=45)
+        self.uniadadF = ttk.Radiobutton(self, text="Fahrenheit", variable=self.tipoUnidad, value="F", command=self.selected).place(x=20, y=70)
+        self.uniadadC = ttk.Radiobutton(self, text="Celsius", variable=self.tipoUnidad, value="C", command=self.selected).place(x=20, y=100)
     
     def start(self):
         self.mainloop()
+    
+    def validateTemperature(self, *args):
+        nuevoValor = self.temperatura.get()
+        try:
+            float(nuevoValor)
+            self.__temperaturaAnt = nuevoValor
+        except:
+            self.temperatura.set(self.__temperaturaAnt)
+# if len(nuevoValor) == 1 and press
+        
+    def selected(self):
+        resultado = 0
+        toUnidad = self.tipoUnidad.get()
+        grados = float(self.temperatura.get())
+            
+        if toUnidad == 'F':
+            resultado = grados * 9/5 + 32
+        elif toUnidad == 'C':
+            resultado = (grados - 32) * 5/9
+        else:
+            resultado = grados
+            
+        self.temperatura.set(resultado)
         
 if __name__ == '__main__':
     app = mainApp()
